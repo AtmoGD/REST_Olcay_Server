c97d41d668d80d9efee140c2c99b6f6001d92c59
Dennis Hawran <dennishawran@gmail.com.de>
2024-05-16 16:31:24 +0200

    try

--- api/get_users.js
+++ api/get_users.js
@@ -1,31 +1,33 @@
-const fs = require("fs");
-const path = require("path");
+// const fs = require("fs");
+// const path = require("path");
 
 // Path to the JSON file
-const DATA_FILE = path.resolve(process.cwd(), "./users.json");
+// const DATA_FILE = path.resolve(process.cwd(), "./users.json");
 
-const data = require("./users.json");
+import data from "./users.json";
 
-const users = {
-  john_doe: {
-    password: "securepassword",
-    devices: ["device123", "device124"],
-  },
-};
+// const users = {
+//   john_doe: {
+//     password: "securepassword",
+//     devices: ["device123", "device124"],
+//   },
+// };
 
 // Load data from JSON file
 function loadData() {
   try {
     return data;
-    const data = fs.readFileSync(DATA_FILE);
-    return JSON.parse(data);
+    // const data = fs.readFileSync(DATA_FILE);
+    // return JSON.parse(data);
   } catch (error) {
-    console.error("Error reading data file:", error);
-    return {};
+    // console.error("Error reading data file:", error);
+    return {
+      error: "Error reading data file:" + error,
+    };
   }
 }
 
-module.exports = async (req, res) => {
+export default async (req, res) => {
   const data = loadData();
   return res.status(200).json(data);
 };
