b63a53739a85c25806f650972792c97f3dfbcf79
Dennis Hawran <dennishawran@gmail.com.de>
2024-05-16 15:42:57 +0200

    try

--- api/get_users.js
+++ api/get_users.js
@@ -4,9 +4,17 @@ const path = require("path");
 // Path to the JSON file
 const DATA_FILE = path.resolve(process.cwd(), "./users.json");
 
+const users = {
+  john_doe: {
+    password: "securepassword",
+    devices: ["device123", "device124"],
+  },
+};
+
 // Load data from JSON file
 function loadData() {
   try {
+    return users;
     const data = fs.readFileSync(DATA_FILE);
     return JSON.parse(data);
   } catch (error) {
